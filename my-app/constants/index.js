ccbee9b858def3c778d73c8b60ce5acd43c0863f
Atharv181 <atharvbobade181@gmail.com>
2023-03-20 12:41:53 +0530

    Frontend completed

---
+++ my-app/constants/index.js
@@ -0,0 +1,67 @@
+export const WHITELIST_CONTRACT_ADDRESS = "0x0CB0603702a757a2233081731A83CCA224796EED";
+
+export const abi = [
+    {
+      "inputs": [
+        {
+          "internalType": "uint8",
+          "name": "_maxNumberOfWhiteListedAddress",
+          "type": "uint8"
+        }
+      ],
+      "stateMutability": "nonpayable",
+      "type": "constructor"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "",
+          "type": "address"
+        }
+      ],
+      "name": "WhiteListedAddresses",
+      "outputs": [
+        {
+          "internalType": "bool",
+          "name": "",
+          "type": "bool"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "addAddressToWhiteList",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "maxNumberOfWhiteListedAddress",
+      "outputs": [
+        {
+          "internalType": "uint8",
+          "name": "",
+          "type": "uint8"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "numberOfWhiteListedAddress",
+      "outputs": [
+        {
+          "internalType": "uint8",
+          "name": "",
+          "type": "uint8"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    }
+  ];
--- my-app/pages/index.js
+++ my-app/pages/index.js
@@ -1,12 +1,131 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
-
-const inter = Inter({ subsets: ['latin'] })
+import Head from "next/head";
+import Web3Modal from "web3modal";
+import styles from "@/styles/Home.module.css";
+import { useEffect, useState, useRef } from "react";
+import { Contract, providers } from "ethers";
+import { WHITELIST_CONTRACT_ADDRESS, abi } from "@/constants";
 
 export default function Home() {
-  const numberOfWhitelisted=0;
+  const [numberOfWhitelisted, setNumberOfWhitelisted] = useState(0);
+  const [walletConnected, setWalletConnected] = useState(false);
+  const web3ModalRef = useRef();
+  const [joinedWhitelist, setJoinedWhitelist] = useState(false);
+  const[loading,setLoading] = useState(false);
+
+  const getProviderOrSigner = async (needSigner = false) => {
+    try {
+      const provider = await web3ModalRef.current.connect();
+      const web3Provider = new providers.Web3Provider(provider);
+
+      const { chainId } = await web3Provider.getNetwork();
+      if (chainId !== 5) {
+        window.alert("Change the network to Goerli");
+        throw new Error("Change network to Goerli");
+      }
+      if (needSigner) {
+        const signer = web3Provider.getSigner();
+        return signer;
+      }
+      return web3Provider;
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const checkIfAddressIsWhitelisted = async () => {
+    try {
+      const signer = getProviderOrSigner(true);
+      const whitelistContract = new Contract(
+        WHITELIST_CONTRACT_ADDRESS,
+        abi,
+        signer
+      );
+      const address = await signer.getAddress();
+      const _joinedWhitelist = await whitelistContract.WhiteListedAddresses(
+        address
+      );
+      setJoinedWhitelist(_joinedWhitelist);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const getNumberOfWhitelisted = async () => {
+    try {
+      const provider = await getProviderOrSigner();
+      const whitelistContract = new Contract(
+        WHITELIST_CONTRACT_ADDRESS,
+        abi,
+        provider
+      );
+      const _numOfWhiteListed =
+        await whitelistContract.numberOfWhiteListedAddress();
+      setNumberOfWhitelisted(_numOfWhiteListed);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const renderButton = () => {
+    if (walletConnected) {
+      if (joinedWhitelist) {
+        return <div>Thanks for joining the whitelist</div>;
+      } else if (loading) {
+        return <p className={styles.button}>Loading...</p>;
+      } else {
+        return (
+          <button onClick={addAddressToWhiteList} className={styles.button}>
+            Join the Whitelist
+          </button>
+        );
+      }
+    } else {
+      return (
+        <button onClick={connectWallet} className={styles.button}>
+          Connect your Wallet
+        </button>
+      );
+    }
+  };
+
+  const addAddressToWhiteList = async () =>{
+    try{
+      const signer = await getProviderOrSigner(true);
+      const whitelistContract = new Contract(
+        WHITELIST_CONTRACT_ADDRESS,
+        abi,
+        signer
+      );
+      const tx = await whitelistContract.addAddressToWhiteList();
+      setLoading(true);
+      await tx.wait();
+      setLoading(false);
+      await getNumberOfWhitelisted();
+      setJoinedWhitelist(true);
+    }catch(err){
+      console.error(err);
+    }
+  }
+  const connectWallet = async () => {
+    try {
+      await getProviderOrSigner();
+      setWalletConnected(true);
+      checkIfAddressIsWhitelisted();
+      getNumberOfWhitelisted();
+    } catch (err) {
+      console.error(err);
+    }
+  };
+  useEffect(() => {
+    if (!walletConnected) {
+      web3ModalRef.current = new Web3Modal({
+        network: "goerli",
+        providerOptions: {},
+        disableInjectedProvider: false,
+      });
+      connectWallet();
+    }
+  }, [walletConnected]);
   return (
     <>
       <Head>
@@ -14,10 +133,14 @@ export default function Home() {
         <meta name="description" content="Whitelist-Dapp" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <div className={styles.header}>
+        <h1 className={styles.title}>WhiteList dApp</h1>
+      </div>
+      <hr />
       <div className={styles.body1}>
         <div className={styles.text}>
-        <h1 className={styles.title}>Welcome to Crypto Devs!</h1>
-        <br />
+          <h1 className={styles.title}>Welcome to Crypto Devs!</h1>
+          <br />
           <div className={styles.description}>
             {/* Using HTML Entities for the apostrophe */}
             It&#39;s an NFT collection for developers in Crypto.
@@ -26,17 +149,17 @@ export default function Home() {
           <div className={styles.description}>
             {numberOfWhitelisted} have already joined the Whitelist
           </div>
-        </div>
-
-        <div>
-        <img className={styles.image} src='./Image.png' alt='Image is here'/>
+          {renderButton()}
         </div>
         
+        <div>
+          <img className={styles.image} src="./Image.png" alt="Image is here" />
+        </div>
       </div>
       <div className={styles.footer}>
         <hr />
         <p>Made with &#10084; by Atharv </p>
       </div>
     </>
-  )
+  );
 }
